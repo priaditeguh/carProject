cd895b26831230ca2a6055a7142f073c4756c305
Priadi Teguh Wibowo <priaditeguh@gmail.com>
2017-10-15 15:03:02 +0200

    push first time

---
+++ test/test.cpp
@@ -0,0 +1,57 @@
+// test.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+#include "opencv2/opencv.hpp"
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	// Create a VideoCapture object and open the input file
+	// If the input is the web camera, pass 0 instead of the video file name
+	VideoCapture cap("car_video_1.mp4");
+
+	// Check if camera opened successfully
+	if (!cap.isOpened()) {
+		cout << "Error opening video stream or file" << endl;
+		return -1;
+	}
+
+	cap.set(CV_CAP_PROP_FPS,3);
+
+	while (1) {
+
+		Mat frame;
+		// Capture frame-by-frame
+		cap >> frame;
+
+		// If the frame is empty, break immediately
+		if (frame.empty())
+			break;
+
+		// Display the resulting frame
+		imshow("Frame", frame);
+
+		std::cout << "FPS: " << cap.get(CV_CAP_PROP_FPS) << std::endl;
+
+		// Press  ESC on keyboard to exit
+		char c = (char)waitKey(25);
+		if (c == 27)
+			break;
+		
+		break;
+	}
+
+	// When everything done, release the video capture object
+	cap.release();
+
+	// Closes all the frames
+	destroyAllWindows();
+
+	return 0;
+}
+
